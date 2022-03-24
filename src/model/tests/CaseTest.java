09a6d37c6089cca15476584c1d24925501968383
Tom PLESKOFF <tompleskoff@gmail.com>
2022-03-24 18:42:58 +0100

    grille

--- src/model/tests/CaseTest.java
+++ src/model/tests/CaseTest.java
@@ -6,7 +6,7 @@ import model.*;
 import org.junit.*;
 public class CaseTest extends TestCase{
 
-    
+
     public void constructWithoutArtifacts() throws Exception{
         Case acase = new Case(new Coord(0,0));
         assertEquals(acase.getCoord().get_x(),0);
